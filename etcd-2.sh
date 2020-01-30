69490ca17dfe05437da0ef0764bc01d3521f988a
suiss64 <59688931+suiss64@users.noreply.github.com>
2020-01-30 17:36:38 +0100

    Update etcd-2.sh

--- etcd-2.sh
+++ etcd-2.sh
@@ -11,11 +11,13 @@ fi
 }
 
 # create snapshot :
-endpoints="https://etcd-0.myperosnalocp.sandbox132.opentlc.com:2379"
+#endpoints="https://etcd-0.myperosnalocp.sandbox132.opentlc.com:2379"
+endpoints="https://etcd-0.crc.testing:2379"
 # snapshot=$(echo snapshot-$(date +"%Y%m%d%H%M%S")) --> remplacer 's3://$(echo $bucketName)/snapshot.db' par 's3://$(echo $bucketName)/$(echo $snapshot)'
 
-# ETCDCTL_API=3 etcdctl --endpoints $(echo $endpoints) --cacert=/etc/ssl/etcd/ca.crt --key=/etc/ssl/etcd/system\:etcd-peer\:etcd-0.myperosnalocp.sandbox132.opentlc.com.key --cert=/etc/ssl/etcd/system\:etcd-peer\:etcd-0.myperosnalocp.sandbox132.opentlc.com.crt snapshot save /tmp/snapshots/snapshot.db && aws s3 cp /tmp/snapshots/snapshot.db s3://$(echo $bucketName)/snapshot.db && rm /tmp/snapshots/snapshot.db && fileName=$(aws s3 ls s3://$(echo $bucketName) --recursive | sort | head -1 | awk '{print $4;}') && backupControl
+#ETCDCTL_API=3 etcdctl --endpoints $(echo $endpoints) --cacert=/etc/ssl/etcd/ca.crt --key=/etc/ssl/etcd/system\:etcd-peer\:etcd-0.myperosnalocp.sandbox132.opentlc.com.key --cert=/etc/ssl/etcd/system\:etcd-peer\:etcd-0.myperosnalocp.sandbox132.opentlc.com.crt snapshot save /tmp/snapshots/snapshot.db && aws s3 cp /tmp/snapshots/snapshot.db s3://$(echo $bucketName)/snapshot.db && rm /tmp/snapshots/snapshot.db && fileName=$(aws s3 ls s3://$(echo $bucketName) --recursive | sort | head -1 | awk '{print $4;}') && backupControl
+ETCDCTL_API=3 etcdctl --endpoints $(echo $endpoints) --cacert=/etc/ssl/etcd/ca.crt --key=/etc/ssl/etcd/system\:etcd-peer\:etcd-0.crc.testing.key --cert=/etc/ssl/etcd/system\:etcd-peer\:etcd-0.crc.testing.crt snapshot save /tmp/snapshots/snapshot.db && aws s3 cp /tmp/snapshots/snapshot.db s3://$(echo $bucketName)/snapshot.db && rm /tmp/snapshots/snapshot.db && fileName=$(aws s3 ls s3://$(echo $bucketName) --recursive | sort | head -1 | awk '{print $4;}') && backupControl
 
-echo "resultat :" && backupControl
+#echo "resultat :" && backupControl
 echo $bucketName
 echo bonjour !
