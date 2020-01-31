790e90882315e9b6d3410ef31839d5e751bf0d6e
suiss64 <59688931+suiss64@users.noreply.github.com>
2020-01-31 10:30:40 +0100

    Update etcd-1.sh

--- etcd-1.sh
+++ etcd-1.sh
@@ -8,7 +8,7 @@ export bucketName=$(echo etcd-backup-eu-central-1-$(date +"%Y%m%d%H%M%S"))
 aws s3 mb s3://$(echo $bucketName)
 
 # add cron job :
-echo "* * * * * /opt/app-root/src/etcd-2.sh" >> /etc/crontab 
+echo "* * * * * root /opt/app-root/src/etcd-2.sh" >> /etc/crontab 
 
 # Test :
 # echo "* * * * * root /opt/app-root/src/etcd-2.sh > /dev/pts/0" >> /etc/crontab
